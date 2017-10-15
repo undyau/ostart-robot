7a4f3ecde12bd7bb13a681d2e414abfd0c3ff50b
Andy Simpson <undyau@users.noreply.github.com>
2017-10-15 17:03:52 +1100

    Introduce Juce/Android Studio consumer

---
+++ AndroidBot/Source/MainComponent.h
@@ -0,0 +1,73 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 5.1.2
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//[Headers]     -- You can add your own extra header files here --
+#include "../JuceLibraryCode/JuceHeader.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class MainComponent  : public Component,
+                       public Button::Listener
+{
+public:
+    //==============================================================================
+    MainComponent ();
+    ~MainComponent();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
+    void resized() override;
+    void buttonClicked (Button* buttonThatWasClicked) override;
+
+    // Binary resources:
+    static const char* satellite512_png;
+    static const int satellite512_pngSize;
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+    ScopedPointer<TextButton> startButton;
+    ScopedPointer<Label> labelTime;
+    ScopedPointer<Label> label;
+    Image cachedImage_satellite512_png_1;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
